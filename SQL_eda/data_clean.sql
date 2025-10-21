f87be2debe22ec689ff15a60a2582e262bffae56
hayden <chang899@purdue.edu>
2025-10-21 01:09:07 -0400

    data clean - delete duplicate

---
+++ SQL_eda/data_clean.sql
@@ -0,0 +1,9 @@
+WITH cte AS (
+	SELECT *,
+		ROW_NUMBER() OVER (PARTITION BY track_id ORDER BY track_name) AS row_number
+	FROM [DS].[dbo].[raw_spotify_song_info.csv]
+)
+DELETE FROM cte WHERE row_number > 1;
+
+DELETE FROM [DS].[dbo].[raw_spotify_song_info.csv]
+WHERE track_name IS NULL OR energy IS NULL;
