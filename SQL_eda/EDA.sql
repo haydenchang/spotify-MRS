267157d5ffcf3ee2e93e1db98c524e51cb5dcbc8
hayden <chang899@purdue.edu>
2025-10-25 18:45:06 -0400

    finished eda

--- SQL_eda/EDA.sql
+++ SQL_eda/EDA.sql
@@ -1,5 +1,4 @@
 -- Data overview
-
 SELECT
 	COUNT(*) AS total_songs,
 	COUNT(DISTINCT artist_name) AS artist_count
@@ -22,8 +21,8 @@ FROM (
 
 	UNION ALL
 	SELECT 'danceability' AS column_name,
-		ROUND(AVG(CAST(danceability AS FLOAT)), 2)			AS avg_value,
-		ROUND(STDEV(CAST(danceability AS FLOAT)), 2)			AS std_value,
+		ROUND(AVG(CAST(danceability AS FLOAT)), 2)		AS avg_value,
+		ROUND(STDEV(CAST(danceability AS FLOAT)), 2)	AS std_value,
 		MIN(danceability)								AS min_value,
 		MAX(danceability)								AS max_value
 	FROM [DS].[dbo].[spotify_song_info.csv]
@@ -102,4 +101,43 @@ FROM (
 ) s
 ORDER BY column_name;
 
--- correlation
+-- Top tracks
+SELECT
+	artist_name,
+	track_name,
+	popularity,
+	RANK() OVER (ORDER BY popularity DESC) AS track_rank
+FROM [DS].[dbo].[spotify_song_info.csv];
+
+-- Top artist
+SELECT
+	artist_name,
+	ROUND(AVG(CAST(popularity AS float)), 2) AS avg_popularity,
+	RANK() OVER (ORDER BY AVG(CAST(popularity AS FLOAT)) DESC) AS artist_rank
+FROM [DS].[dbo].[spotify_song_info.csv]
+GROUP BY artist_name
+ORDER BY artist_rank;
+
+-- Outlier
+WITH stats AS (
+	SELECT
+		AVG(CAST(duration_ms AS FLOAT)) AS avg_duration,
+		STDEV(CAST(duration_ms AS FLOAT)) AS std_duration
+	FROM [DS].[dbo].[spotify_song_info.csv]
+)
+SELECT *
+FROM (
+	SELECT
+		spotify.track_name,
+		spotify.artist_name,
+		spotify.duration_ms,
+		CASE
+			WHEN CAST(spotify.duration_ms AS FLOAT) > s.avg_duration + 3 * s.std_duration THEN 'High Outlier'
+			WHEN CAST(spotify.duration_ms AS FLOAT) < s.avg_duration - 3 * s.std_duration THEN 'Low Outlier'
+		END AS outlier_flag
+	FROM [DS].[dbo].[spotify_song_info.csv] AS spotify
+	CROSS JOIN stats AS s
+) AS result
+WHERE outlier_flag IS NOT NULL
+ORDER BY duration_ms ASC;
+
